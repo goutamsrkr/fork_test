0549e398d7f57b950aef4bc7cc92db022101be3e
Goutam Sarkar <gsarkar@bmc.com>
2020-09-11 20:48:01 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+data "aws_vpc" "default_vpc1" {
+  default = true
+}
+
+data "aws_subnet_ids" "default_subnet_ids"{
+  vpc_id = data.aws_vpc.default_vpc1.id
+}
+
+output "vpc_info" {
+  value = data.aws_subnet_ids.default_subnet_ids.ids
+}
+
+
 variable "region_list" {
   type = list(string)
   default = ["asia", "europe", "usa"]
